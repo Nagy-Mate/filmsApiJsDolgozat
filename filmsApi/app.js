2c0cba56e656141f1f8eb97abf3d2e1769365762
Nagy Máté <b1nagmat@vasvari.org>
2025-03-10 12:56:00 +0100

    done

--- filmsApi/app.js
+++ filmsApi/app.js
@@ -5,51 +5,52 @@ const app = express();
 app.use(express.json());
 
 app.get("/movies", (req, res)=>{
-    res.json(films);
+    res.status(200).json(films);
 });
 
 app.get("/movies/:id", (req, res)=>{
     const id = req.params.id;
     if(id < 0 || id > films.length - 1){
-        res.status(404, {message: 'Invalid id! '})
+        res.status(404).json({Message: 'Invalid id! '});
     }
-    res.json(films[id]);
+    res.status(200).json(films[id]);
 });
 
 app.post("/movies", (req, res)=>{
     const {title, director, year, oszkar} = req.body;
-    if(!title || !director || !year){
-        res.status(404, {message: 'Some data missing! '})
+    if(!title || !director || !year || oszkar == null){
+        res.status(404).json({message: 'Some data missing! '});
     }
+
     const newFilm = {title, director, year, oszkar};
     films.push(newFilm);
-    res.json(newFilm);
+    res.status(200).json(newFilm);
 });
 
 app.put("/movies/:id", (req, res)=>{
     const id = req.params.id;
     if(id < 0 || id > films.length - 1){
-        res.status(404, {message: 'Invalid is! '})
+        res.status(404).json({message: 'Invalid id! '});
     }
 
     const {title, director, year, oszkar} = req.body;
-    if(!title || !director || !year){
-        res.status(404, {message: 'Some data missing! '})
+    if(!title || !director || !year || oszkar == null){
+        res.status(404).json({message: 'Some data missing! '});
     }
 
     films[id] = {title, director, year, oszkar};
-    res.json(films[id]);
+    res.status(200).json(films[id]);
 });
 
 
 app.delete("/movies/:id", (req, res)=>{
     const id = req.params.id;
     if(id < 0 || id > films.length - 1){
-        res.status(404, {message: 'Invalid id! '})
+        res.status(404).json({message: 'Invalid id! '});
     }
 
     films.splice(id, 1);
-    res.json({message: "Deleted successfully"});
+    res.status(200).json({message: "Deleted successfully"});
 });
 
 app.listen(3000, (req, res)=>{
